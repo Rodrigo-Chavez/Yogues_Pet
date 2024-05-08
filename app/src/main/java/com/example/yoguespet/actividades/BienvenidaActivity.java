3c5d77907bdd738213f9d7f4326f6c888b0bdae7
Sebas6422 <91403654+Sebas6422@users.noreply.github.com>
2024-05-08 12:19:12 -0500

    owo

--- app/src/main/java/com/example/yoguespet/actividades/BienvenidaActivity.java
+++ app/src/main/java/com/example/yoguespet/actividades/BienvenidaActivity.java
@@ -2,15 +2,32 @@ package com.example.yoguespet.actividades;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.yoguespet.R;
 
-public class BienvenidaActivity extends AppCompatActivity {
+public class BienvenidaActivity extends AppCompatActivity implements View.OnClickListener {
 
+    TextView txtRecuperar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bienvenida);
+
+        txtRecuperar = findViewById(R.id.OlvidasteContra);
+
+        txtRecuperar.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.OlvidasteContra){
+            Intent iRecuperar = new Intent(this, RecuperarCuentaActivity.class);
+            startActivity(iRecuperar);
+            finish();
+        }
     }
 }
--- app/src/main/java/com/example/yoguespet/actividades/RecuperarCuentaActivity.java
+++ app/src/main/java/com/example/yoguespet/actividades/RecuperarCuentaActivity.java
@@ -2,15 +2,49 @@ package com.example.yoguespet.actividades;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.yoguespet.R;
 
-public class RecuperarCuentaActivity extends AppCompatActivity {
+public class RecuperarCuentaActivity extends AppCompatActivity implements View.OnClickListener{
+
+    TextView txtRecuperarnum;
+    ImageView imgVolver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recuperar_cuenta);
+
+        txtRecuperarnum = findViewById(R.id.RecuperarConNum);
+        imgVolver = findViewById(R.id.imgVolver);
+        
+        txtRecuperarnum.setOnClickListener(this);
+        imgVolver.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.RecuperarConNum){
+            recuperarCuentaNum();
+        } else if (v.getId() == R.id.imgVolver) {
+            volver();
+        }
+    }
+
+    private void volver() {
+        Intent iVolver = new Intent(this, BienvenidaActivity.class);
+        startActivity(iVolver);
+        finish();
+    }
+
+    private void recuperarCuentaNum() {
+        Intent iRecuperarNum = new Intent(this, RecuperarCuentaNumActivity.class);
+        startActivity(iRecuperarNum);
+        finish();
     }
 }
